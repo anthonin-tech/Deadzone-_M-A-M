c235a7e62c1321b30eed77aa5229f44efa257890
anthonin-tech <anthonin.chesneau08@gmail.com>
2026-01-29 17:52:36 +0100

    ajout de l'inventaire et de petites fonctionnalités

--- sprites/item.py
+++ sprites/item.py
@@ -1,27 +1,81 @@
 import pickle
 
 class Items:
-    def __init__(self, name, durability, category, rarity, quantity=1):
+    def __init__(self, name, durability, category, rarity, illustration, quantity=1):
         self._name = name 
         self._durability = durability
         self._category = category
         self._rarity = rarity
         self._quantity = quantity
+        self._illustration = illustration
+    
+    def __str__(self):
+        return (f"{self._name} \n| Durabilité : {self._durability} \n| Catégorie : {self._category} \n| Rareté : {self._rarity} \n| Quantité : {self._quantity}\n")
+    
+    def to_dict_item(self):
+        return {
+            'name': self._name,
+            'durability': self._durability,
+            'category': self._category,
+            'rarity': self._rarity,
+            'quantity': self._quantity
+        }
+
+    def is_same_item(self, other):
+        return(self._name == other._name and self._category == other._category and self._rarity == other._rarity)
+    
+class Inventory:
+    def __init__(self):
         self._inventory = []
 
-    def save(self):
+    def __str__(self):
+        if not self._inventory:
+            return "Inventaire Vide"
 
-        for key, value in self.__dict__.items():
-            if key != "_inventory":
-                self._inventory.append(value)
-
-        print(self._inventory)
-
+        texte = "\n Inventaire du joueur :\n"
+        for item in self._inventory:
+            texte += f"• {item}\n"
+        return texte     
     
-player_inventory = [Items("Fusil à pompe", 100, "arme", "rare"), Items("Soin", 1, "soin", "légendaire"), Items("Bouclier" , 30, "Armure", "épique")]
+    def add_item(self, name, durability, category, rarity, illustration, quantity=1,):
 
-with open("save.pkl", "wb") as f:
-    pickle.dump(player_inventory, f)
+        new_item = Items(name, durability, category, rarity, illustration, quantity)
 
-with open("save.pkl", "rb") as f:
-    loaded_inventory = pickle.load(f)
+        for item in self._inventory:
+            if item.is_same_item(new_item):
+                item._quantity += 1
+                print(f"• {name} x {quantity} ajouté (Total: {item._quantity})")
+                return
+        
+        self._inventory.append(new_item)
+        print(f"• Nouveau {name} ajouté")
+    
+    def drop_item(self, name, durability, category, rarity, illustration, quantity=1):
+
+        drop_item = Items(name, durability, category, rarity, illustration, quantity)
+
+        for item in self._inventory:
+            if item.is_same_item(drop_item):
+                item._quantity -= 1
+                if item._quantity <= 0:
+                    self._inventory.remove(item)
+                print(f"• {name} x {quantity} supprimé (Total: {item._quantity})")
+                return
+
+if __name__ == "__main__": 
+  
+    player = Inventory()
+    player.add_item("Fusil à pompe", 100, "arme", "rare", "shotgun.png")
+    player.add_item("Fusil à pompe", 100, "arme", "rare", "shotgun.png")
+    player.add_item("Bouclier", 30, "Armure", "épique", "shield.png")
+    player.add_item("Fusil à pompe", 100, "arme", "rare", "shotgun.png")
+    player.add_item("Soin", 1, "soin", "légendaire", "potion_legendary.png")
+    player.add_item("Bouclier", 30, "Armure", "épique", "shield.png")
+    player.add_item("Soin", 1, "soin", "légendaire", "potion_legendary.png")
+    player.add_item("Soin", 1, "soin", "légendaire", "potion_legendary.png")
+
+    player.drop_item("Soin", 1, "soin", "légendaire", "potion_legendary.png")
+    player.drop_item("Soin", 1, "soin", "légendaire", "potion_legendary.png")
+    
+    print(player)
+
