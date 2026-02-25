c5c3850ca614ddaa107e960e432815b682583b05
Maelys-B <maelysbart@gmail.com>
2026-02-25 23:13:05 +0100

    Gestion des collisions

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+from sprites.player import Player 
 import pygame
 import pytmx
 import pyscroll
@@ -14,18 +15,55 @@ class Game:
         map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
         map_layer.zoom = 2 
 
-        self.group = pyscroll.PyscrollGroup(map_layer = map_layer, default_layer = 1)
+        player_position = tmx_data.get_object_by_name('Player')
+        self.player = Player(player_position.x, player_position.y)
+
+        self.walls = []
+
+        for obj in tmx_data.objects:
+            if obj.type == "collision":
+                self.walls.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
+
+        self.group = pyscroll.PyscrollGroup(map_layer = map_layer, default_layer = 13)
+        self.group.add(self.player)
+
+    def handle_input(self):
+        pressed = pygame.key.get_pressed()
+
+        if pressed[pygame.K_UP]:
+            self.player.move_up()
+        elif pressed[pygame.K_DOWN]:
+            self.player.move_down()
+        elif pressed[pygame.K_LEFT]:
+            self.player.move_left()
+        elif pressed[pygame.K_RIGHT]:
+            self.player.move_right()
+
+    def update(self):
+        self.group.update()
+
+        for sprite in self.group.sprites():
+            if sprite.feet.collidelist(self.walls) > -1:
+                sprite.move_back()
 
     def run(self):
+
+        clock = pygame.time.Clock()
+
         running = True
 
         while running:
 
+            self.player.save_location()
+            self.handle_input()
+            self.update()
+            self.group.center(self.player.rect.center)
             self.group.draw(self.screen)
             pygame.display.flip()
             
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+            clock.tick(60)
 
         pygame.quit()
